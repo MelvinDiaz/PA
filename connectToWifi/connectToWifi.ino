6442c7bd064994f9f92c42cb9cc96e0dcc7f3d22
MelvinDiaz <00008821@uca.edu.sv>
2023-09-18 21:03:57 -0600

    Add: connection to wifi esp8266

---
+++ connectToWifi/connectToWifi.ino
@@ -0,0 +1,27 @@
+#include <ESP8266WiFi.h>
+#define ssid "Hackerman"
+#define password "dmnq5761"
+
+void setup()
+{
+  // put your setup code here, to run once:
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.begin(115200);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print("Connection being made, wait please");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to: ");
+  Serial.println(ssid);
+  Serial.print("IP Address:");
+  Serial.println(WiFi.localIP());
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
